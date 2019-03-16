72bba143a0c984fbcf8575c967591749b4c8919c
Ram <mhram0208@gmail.com>
2019-03-16 17:07:19 +0530

    My first git commit

--- demo/src/main/java/com/example/controller/BooksController.java
+++ demo/src/main/java/com/example/controller/BooksController.java
@@ -3,8 +3,12 @@ package com.example.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.dto.BookDTO;
@@ -15,12 +19,37 @@ public class BooksController {
 
 	@Autowired
 	private BookService bookService;
-	
-	@RequestMapping(value="/api/v1/books",method=RequestMethod.GET)
-	public List<BookDTO> getAllBooks(){
+
+	@RequestMapping(value = "/api/v1/books", method = RequestMethod.GET)
+	public ResponseEntity<?> getAllBooks(@RequestParam("sortBy") String sortBy) {
+		List<BookDTO> books = bookService.getAllBooks(sortBy);
+		if (books == null || books.isEmpty()) {
+			return new ResponseEntity<String>("No Books Available", HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<List>(books, HttpStatus.OK);
+		}
+
+	}
+
+	@RequestMapping(value = "/api/v1/books/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> getBook(@PathVariable("id") Integer id) {
 		
-		return bookService.getAllBooks();
-		
+		BookDTO book = bookService.getBook(id);
+		if (book == null) {
+			return new ResponseEntity<String>("No Book Available", HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<BookDTO>(book, HttpStatus.OK);
+		}
 	}
 	
+	@RequestMapping(value="/api/v1/books/author/{authorName}", method = RequestMethod.GET)
+	public ResponseEntity<?> getBook(@PathVariable("authorName") String authorName){
+		List<BookDTO> books=bookService.getBook(authorName);
+		if(books == null || books.isEmpty()) {
+			return new ResponseEntity<String>("This author has no books", HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<List>(books, HttpStatus.OK);
+		}
+	}
+
 }
--- demo/src/main/java/com/example/service/impl/BookServiceImpl.java
+++ demo/src/main/java/com/example/service/impl/BookServiceImpl.java
@@ -1,11 +1,13 @@
 package com.example.service.impl;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.example.dao.BooksRepository;
@@ -15,23 +17,54 @@ import com.example.service.BookService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
-public class BookServiceImpl implements BookService{
+public class BookServiceImpl implements BookService {
 
 	@Autowired
 	private BooksRepository bookRepository;
-	
+
 	@Autowired
 	private ObjectMapper objectMapper;
-	
+
 	@PostConstruct
-	void populate(){
-		bookRepository.save(new Book(1,"One indian girl","Chetan Bhagat"));
+	void populate() {
+		bookRepository.save(new Book(1, "One indian girl", "Chetan"));
+		bookRepository.save(new Book(2, "Three mistakes of my life", "Chetan"));
+		bookRepository.save(new Book(3, "ABCD", "Sumant"));
+		bookRepository.save(new Book(4, "XYZ", "Ram"));
 	}
+
 	@Override
-	public List<BookDTO> getAllBooks() {
+	public List<BookDTO> getAllBooks(String sortBy) {
 		// TODO Auto-generated method stub
-		List<Book> books=bookRepository.findAll();
-		return books.stream().map(book->objectMapper.convertValue(book, BookDTO.class)).collect(Collectors.toList());
+		Sort sort = null;
+		List<Book> books;
+		if (sortBy != null && sortBy.startsWith("-")) {
+			sort = new Sort(Sort.Direction.DESC, sortBy.substring(1, sortBy.length()));
+		} else if (sortBy != null) {
+			sort = new Sort(Sort.Direction.ASC, sortBy.substring(0, sortBy.length()));
+		}
+		if (sort != null) {
+			books = bookRepository.findAll(sort);
+		} else {
+			books = bookRepository.findAll();
+		}
+		return books.stream().map(book -> objectMapper.convertValue(book, BookDTO.class)).collect(Collectors.toList());
+	}
+
+	@Override
+	public BookDTO getBook(Integer id) {
+		Optional<Book> book = bookRepository.findById(id);
+		BookDTO bookDTO = null;
+		if (book.isPresent()) {
+			bookDTO = objectMapper.convertValue(book, BookDTO.class);
+		}
+		return bookDTO;
+	}
+
+	@Override
+	public List<BookDTO> getBook(String authorName) {
+		List<Book> books = bookRepository.findByAuthorName(authorName);
+		return books.stream().map(book -> objectMapper.convertValue(book, BookDTO.class)).collect(Collectors.toList());
 	}
 
 }
